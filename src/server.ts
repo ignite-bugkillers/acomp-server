90779a741afe2cda85d7765cefb02d959f9906a7
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-14 18:54:27 -0300

    style: add style guide

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import express from 'express'
-import cors from 'cors'
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (request, response) => {
   return response.json({
-    message: 'hello word'
-  })
-})
+    message: 'hello word',
+  });
+});
 
-app.listen(3333, () => console.log('Server is running!'))
+app.listen(3333, () => console.log('Server is running!'));
