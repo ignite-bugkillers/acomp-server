cb91d181399c4dc204b62523ecb93eb208755720
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-15 20:34:12 -0300

    feat: add User respositories

---
+++ src/modules/accounts/repositories/in-memory/InMemoryUsersRepository.ts
@@ -0,0 +1,35 @@
+import { User } from '../../entities/User';
+import { ICreateUserDTO } from '../../useCases/createUser/ICreateUserDTO';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class InMemoryUsersRepository implements IUsersRepository {
+  private users: User[];
+
+  constructor() {
+    this.users = [];
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    return this.users.find((findUser) => findUser.email === email);
+  }
+
+  public async create({
+    name,
+    email,
+    password,
+  }: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      password,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
+  }
+}
