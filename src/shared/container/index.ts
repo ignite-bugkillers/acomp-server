7464a17bf669cb86210cbb9edeca66a93d05f506
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-17 18:59:15 -0300

    feat: add dependency injection for patients respositories

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -4,8 +4,15 @@ import './providers';
 
 import { TypeormUsersRepository } from '../../modules/accounts/repositories/implementations/TypeormUsersRepositor';
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
+import { TypeormPatientsRepository } from '../../modules/patients/repositories/implementations/TypeormPatientsRepository';
+import { IPatientsRepository } from '../../modules/patients/repositories/IPatientsRepository';
 
 container.registerSingleton<IUsersRepository>(
   'TypeormUsersRepository',
   TypeormUsersRepository
+);
+
+container.registerSingleton<IPatientsRepository>(
+  'TypeormPatientsRepository',
+  TypeormPatientsRepository
 );
