fa35e090cf0242fb107282293cc890cb6db8d8f7
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-17 18:53:49 -0300

    feat: add patients repositories

---
+++ src/modules/patients/repositories/IPatientsRepository.ts
@@ -0,0 +1,7 @@
+import { Patient } from '../entities/Patient';
+import { ICreatePatientDTO } from '../useCases/createPatient/ICreatePatientDTO';
+
+export interface IPatientsRepository {
+  create(data: ICreatePatientDTO): Promise<Patient>;
+  findByCpf(cpf: string): Promise<undefined | Patient>;
+}
