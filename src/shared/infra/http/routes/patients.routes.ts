f62857788ed32247d47d7ade22b66bd2d260cb16
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-17 19:56:32 -0300

    feat: add list all patients route

--- src/shared/infra/http/routes/patients.routes.ts
+++ src/shared/infra/http/routes/patients.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import { CreatePatientController } from '../../../../modules/patients/useCases/createPatient/CreatePatientController';
+import { ListAllPatientsController } from '../../../../modules/patients/useCases/listAllPatients/ListAllPatientsController';
 
 const patientsRoutes = Router();
 
-const createPatientsController = new CreatePatientController();
+const createPatientController = new CreatePatientController();
+const listAllPatientsController = new ListAllPatientsController();
 
-patientsRoutes.post('/', createPatientsController.handler);
+patientsRoutes.get('/', listAllPatientsController.handler);
+patientsRoutes.post('/', createPatientController.handler);
 
 export { patientsRoutes };
