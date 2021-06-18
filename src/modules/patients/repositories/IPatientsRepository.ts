0a61fa44e06a11e0fe7eebdf43b7820e8268fcb6
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-18 19:58:53 -0300

    feat: update patients repository

--- src/modules/patients/repositories/IPatientsRepository.ts
+++ src/modules/patients/repositories/IPatientsRepository.ts
@@ -3,6 +3,8 @@ import { ICreatePatientDTO } from '../useCases/createPatient/ICreatePatientDTO';
 
 export interface IPatientsRepository {
   create(data: ICreatePatientDTO): Promise<Patient>;
+  save(patient: Patient): Promise<Patient>;
   findByCpf(cpf: string): Promise<undefined | Patient>;
+  findById(id: string): Promise<undefined | Patient>;
   listAll(): Promise<Patient[]>;
 }
