b31cd1f7f95584bba08f2db3d16ccda2598934af
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-18 20:14:10 -0300

    feat: update patients routes

--- src/shared/infra/http/routes/patients.routes.ts
+++ src/shared/infra/http/routes/patients.routes.ts
@@ -2,13 +2,16 @@ import { Router } from 'express';
 
 import { CreatePatientController } from '../../../../modules/patients/useCases/createPatient/CreatePatientController';
 import { ListAllPatientsController } from '../../../../modules/patients/useCases/listAllPatients/ListAllPatientsController';
+import { UpdatePatientController } from '../../../../modules/patients/useCases/updatePatient/UpdatePatientController';
 
 const patientsRoutes = Router();
 
 const createPatientController = new CreatePatientController();
 const listAllPatientsController = new ListAllPatientsController();
+const updatePatientController = new UpdatePatientController();
 
 patientsRoutes.get('/', listAllPatientsController.handler);
 patientsRoutes.post('/', createPatientController.handler);
+patientsRoutes.put('/:id', updatePatientController.handle);
 
 export { patientsRoutes };
