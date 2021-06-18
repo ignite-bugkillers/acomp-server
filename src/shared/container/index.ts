ae556b819db3cc4614c08d5f8944e1bb2217d07c
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2021-06-17 23:31:34 -0300

    feat: add doctor repository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,15 @@ import './providers';
 import { TypeormUsersRepository } from '../../modules/accounts/repositories/implementations/TypeormUsersRepositor';
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 
+import { TypeormDoctorRepository } from '../../modules/doctors/repositories/implementations/TypeormDoctorRepository';
+import { IDoctorRepository } from '../../modules/doctors/repositories/interfaces/IDoctorRepository';
+
 container.registerSingleton<IUsersRepository>(
   'TypeormUsersRepository',
   TypeormUsersRepository
+);
+
+container.registerSingleton<IDoctorRepository>(
+  'TypeormDoctorRepository',
+  TypeormDoctorRepository
 );
