dc80c615009556dfded960674be1b697730e6f78
caiocampos-hotmart <82822777+caiocampos-hotmart@users.noreply.github.com>
2021-06-17 23:32:45 -0300

    feat: add authentication middleware

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 import { authenticateRoutes } from './authenticate.routes';
+import { doctorRoutes } from './doctors.routes';
 import { usersRoutes } from './users.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRoutes);
 routes.use(authenticateRoutes);
+routes.use(ensureAuthenticated);
+
+routes.use('/doctors', doctorRoutes);
 
 export { routes };
