f7d1bfe530f0ffe2e765d02a8039829f5989d615
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-19 15:41:52 -0300

    feat: add list patients use case

--- src/modules/patients/useCases/listPatients/ListPatientsUseCase.ts
+++ src/modules/patients/useCases/listPatients/ListPatientsUseCase.ts
@@ -2,15 +2,20 @@ import { inject, injectable } from 'tsyringe';
 
 import { Patient } from '../../entities/Patient';
 import { IPatientsRepository } from '../../repositories/IPatientsRepository';
+import { IListPatientsDTO } from './IListPatientsDTO';
 
 @injectable()
-export class ListAllPatientsUseCase {
+export class ListPatientsUseCase {
   constructor(
     @inject('TypeormPatientsRepository')
     private patientsRepository: IPatientsRepository
   ) {}
 
-  public async execute(): Promise<Patient[]> {
+  public async execute({ patient_name }: IListPatientsDTO): Promise<Patient[]> {
+    if (patient_name) {
+      return this.patientsRepository.findByName(patient_name);
+    }
+
     return this.patientsRepository.listAll();
   }
 }
