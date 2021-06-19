b6c0abbceae5575e2eff78c3664e5b6c77248cfb
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-19 15:43:45 -0300

    fix: update patients routes for filter patients by name

--- src/shared/infra/http/routes/patients.routes.ts
+++ src/shared/infra/http/routes/patients.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 
 import { CreatePatientController } from '../../../../modules/patients/useCases/createPatient/CreatePatientController';
-import { ListAllPatientsController } from '../../../../modules/patients/useCases/listAllPatients/ListAllPatientsController';
+import { ListPatientsController } from '../../../../modules/patients/useCases/listPatients/ListPatientsController';
 import { UpdatePatientController } from '../../../../modules/patients/useCases/updatePatient/UpdatePatientController';
 
 const patientsRoutes = Router();
 
 const createPatientController = new CreatePatientController();
-const listAllPatientsController = new ListAllPatientsController();
+const listPatientsController = new ListPatientsController();
 const updatePatientController = new UpdatePatientController();
 
-patientsRoutes.get('/', listAllPatientsController.handle);
+patientsRoutes.get('/', listPatientsController.handle);
 patientsRoutes.post('/', createPatientController.handle);
 patientsRoutes.put('/:id', updatePatientController.handle);
 
