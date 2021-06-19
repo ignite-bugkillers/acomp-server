5cb4937e77e0026b80ffd8f074297e174a70fa1c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-19 17:56:38 -0300

    feat: add show patient route

--- src/shared/infra/http/routes/patients.routes.ts
+++ src/shared/infra/http/routes/patients.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { CreatePatientController } from '../../../../modules/patients/useCases/createPatient/CreatePatientController';
 import { ListPatientsController } from '../../../../modules/patients/useCases/listPatients/ListPatientsController';
+import { ShowPatientController } from '../../../../modules/patients/useCases/showPatient/ShowPatientController';
 import { UpdatePatientController } from '../../../../modules/patients/useCases/updatePatient/UpdatePatientController';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -10,11 +11,13 @@ const patientsRoutes = Router();
 const createPatientController = new CreatePatientController();
 const listPatientsController = new ListPatientsController();
 const updatePatientController = new UpdatePatientController();
+const showPatientController = new ShowPatientController();
 
 patientsRoutes.use(ensureAuthenticated);
 
 patientsRoutes.get('/', listPatientsController.handle);
 patientsRoutes.post('/', createPatientController.handle);
 patientsRoutes.put('/:id', updatePatientController.handle);
+patientsRoutes.get('/:id', showPatientController.handle);
 
 export { patientsRoutes };
