76bad7f03554feee424d8b0f390c449d8ade2606
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-20 12:44:03 -0300

    fix: rename parameter id to crm from findByCRM

--- src/modules/doctors/repositories/interfaces/IDoctorRepository.ts
+++ src/modules/doctors/repositories/interfaces/IDoctorRepository.ts
@@ -5,6 +5,6 @@ export interface IDoctorRepository {
   create(data: ICreateDoctorDTO): Promise<Doctor>;
   save(doctor: Doctor): Promise<Doctor>;
   findByID(id: string): Promise<Doctor | undefined>;
-  findByCRM(id: string): Promise<Doctor | undefined>;
+  findByCRM(crm: string): Promise<Doctor | undefined>;
   findAll(): Promise<Doctor[] | undefined>;
 }
