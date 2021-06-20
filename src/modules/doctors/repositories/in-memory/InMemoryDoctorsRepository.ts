d48e0249a77809f114050ebbf0929b871cfba40f
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-20 12:53:24 -0300

    feat: add in memory doctors repository

---
+++ src/modules/doctors/repositories/in-memory/InMemoryDoctorsRepository.ts
@@ -0,0 +1,53 @@
+import { Doctor } from '../../entities/Doctor';
+import { ICreateDoctorDTO } from '../../useCases/createDoctor/ICreateDoctorDTO';
+import { IDoctorRepository } from '../interfaces/IDoctorRepository';
+
+export class InMemoryDoctorsRepository implements IDoctorRepository {
+  private doctors: Doctor[];
+
+  constructor() {
+    this.doctors = [];
+  }
+
+  public async create({
+    crm,
+    name,
+    phone,
+    user_id,
+  }: ICreateDoctorDTO): Promise<Doctor> {
+    const doctor = new Doctor();
+
+    Object.assign(doctor, {
+      crm,
+      name,
+      phone,
+      user_id,
+    });
+
+    this.doctors.push(doctor);
+
+    return doctor;
+  }
+
+  public async save(doctor: Doctor): Promise<Doctor> {
+    const doctorIndex = this.doctors.findIndex(
+      (findDoctor) => findDoctor.id === doctor.id
+    );
+
+    this.doctors[doctorIndex] = doctor;
+
+    return doctor;
+  }
+
+  public async findByID(id: string): Promise<Doctor | undefined> {
+    return this.doctors.find((findDoctor) => findDoctor.id === id);
+  }
+
+  public async findByCRM(crm: string): Promise<Doctor | undefined> {
+    return this.doctors.find((findDoctor) => findDoctor.crm === crm);
+  }
+
+  public async findAll(): Promise<Doctor[]> {
+    return this.doctors;
+  }
+}
