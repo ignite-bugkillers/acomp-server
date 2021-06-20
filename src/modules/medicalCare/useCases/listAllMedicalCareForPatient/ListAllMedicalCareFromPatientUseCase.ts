1c22c1df64c26b6a1e919e13e07dad2c59f2fad1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-20 16:43:55 -0300

    test: add list all medical care from patient

--- src/modules/medicalCare/useCases/listAllMedicalCareForPatient/ListAllMedicalCareFromPatientUseCase.ts
+++ src/modules/medicalCare/useCases/listAllMedicalCareForPatient/ListAllMedicalCareFromPatientUseCase.ts
@@ -1,16 +1,27 @@
 import { inject, injectable } from 'tsyringe';
 
+import { IPatientsRepository } from '../../../patients/repositories/IPatientsRepository';
 import { IMedicalCareRepository } from '../../repositories/IMedicalCateRepository';
 import { IListAllMedicalCareFromPatientDTO } from './IListAllMedicalCareFromPatientDTO';
+import { ListAllMedicalCareFromPatientError } from './ListAllMedicalCareFromPatientError';
 
 @injectable()
 export class ListAllMedicalCareFromPatientUseCase {
   constructor(
     @inject('TypeormMedicalCareRepository')
-    private medicalCareRepository: IMedicalCareRepository
+    private medicalCareRepository: IMedicalCareRepository,
+
+    @inject('TypeormPatientsRepository')
+    private patientsRepository: IPatientsRepository
   ) {}
 
   public async execute({ patient_id }: IListAllMedicalCareFromPatientDTO) {
+    const patient = await this.patientsRepository.findById(patient_id);
+
+    if (!patient) {
+      throw new ListAllMedicalCareFromPatientError();
+    }
+
     return this.medicalCareRepository.findAllFromPatient(patient_id);
   }
 }
