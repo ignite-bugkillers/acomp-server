06699351c7cb831a010754c763f523b3dae0f1ff
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-20 14:24:59 -0300

    feat: add dependency injection for medical care respositories

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,6 +6,8 @@ import { TypeormUsersRepository } from '../../modules/accounts/repositories/impl
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { TypeormDoctorRepository } from '../../modules/doctors/repositories/implementations/TypeormDoctorRepository';
 import { IDoctorRepository } from '../../modules/doctors/repositories/interfaces/IDoctorRepository';
+import { IMedicalCareRepository } from '../../modules/medicalCare/repositories/IMedicalCateRepository';
+import { TypeormMedicalCareRepository } from '../../modules/medicalCare/repositories/implementations/TypeormMedicalCareRepository';
 import { TypeormPatientsRepository } from '../../modules/patients/repositories/implementations/TypeormPatientsRepository';
 import { IPatientsRepository } from '../../modules/patients/repositories/IPatientsRepository';
 import { TypeormProcedureRepository } from '../../modules/procedures/repositories/implementations/TypeormProcedureRepository';
@@ -29,4 +31,9 @@ container.registerSingleton<IPatientsRepository>(
 container.registerSingleton<IProcedureRepository>(
   'TypeormProcedureRepository',
   TypeormProcedureRepository
+);
+
+container.registerSingleton<IMedicalCareRepository>(
+  'TypeormMedicalCareRepository',
+  TypeormMedicalCareRepository
 );
