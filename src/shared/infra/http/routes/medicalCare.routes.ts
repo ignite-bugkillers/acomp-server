3587d68aaf68583a9b01811913d4b97687a99810
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-06-20 15:04:43 -0300

    feat: add findAllFromPatient to medical care route

--- src/shared/infra/http/routes/medicalCare.routes.ts
+++ src/shared/infra/http/routes/medicalCare.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
 
 import { CreateMedicalCareController } from '../../../../modules/medicalCare/useCases/createMedicalCare/CreateMedicalCareController';
+import { ListAllMedicalCareFromPatientController } from '../../../../modules/medicalCare/useCases/listAllMedicalCareForPatient/ListAllMedicalCareFromPatientController';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const medicalCareRoutes = Router();
 
 const createMedicalCareController = new CreateMedicalCareController();
+const listAllMedicalCareFromPatientController =
+  new ListAllMedicalCareFromPatientController();
 
 medicalCareRoutes.use(ensureAuthenticated);
 
+medicalCareRoutes.get('/', listAllMedicalCareFromPatientController.handle);
 medicalCareRoutes.post('/', createMedicalCareController.handle);
 
 export { medicalCareRoutes };
